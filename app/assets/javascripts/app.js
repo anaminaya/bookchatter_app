6a9b9130fc6bbf97066286bb68f5e07c5f7ec343
Ana Minaya <analizetteminaya@gmail.com>
2016-09-24 16:40:37 -0400

    added autocomplete for genre withing search bar

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,34 @@
 (function() {
   'use strict';
 
-  angular.module("app", []);
+  var myApp = angular.module("app", []);
+
+  myApp.filter('returnFavorites', function () {
+  return function (chats, filterOn) {
+  	if(!filterOn){
+  		return chats;
+  	}
+   	else{
+   		var favorites = chats.filter(function(chat){
+      	return chat.favorite;
+    	});
+      return favorites;
+   }
+  };
+});
+
+function favoritechatroomCtrl($scope) {
+	$scope.filterOn = true;
+  $scope.chats =
+    [
+    {name: "Chat1", favorite: false},
+    {name: "Chat2", favorite: true},
+    {name: "Chat3", favorite: false},
+    {name: "Chat4", favorite: false},
+    {name: "Chat5", favorite: false},
+    {name: "Chat6", favorite: false},
+  ];
+
+
+}
 })();
